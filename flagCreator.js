775a7e2333b488df6fc585df6cb0e8ac9357cdc5
root <root@rickey-OptiPlex-745.(none)>
2013-11-01 14:00:01 -0400

    about to reset the computer, saving changes

--- flagCreator.js
+++ flagCreator.js
@@ -84,25 +84,25 @@ var paintInfo =
                             {
                                 "type": "rect",
                                 "width": 1,
-                                "height": 0.3,
+                                "height": (1/3),
                                 "x": 0,
-                                "y": 0.6
+                                "y": (2/3)
                             }
                         ],
                         [
                             {
                                 "type": "rect",
                                 "width": 1,
-                                "height": 0.3,
+                                "height": (1/3),
                                 "x": 0,
-                                "y": 0.3
+                                "y": (1/3)
                             }
                         ],
                         [
                             {
                                 "type": "rect",
                                 "width": 1,
-                                "height": 0.3,
+                                "height": (1/3),
                                 "x": 0,
                                 "y": 0
                             }
@@ -116,25 +116,25 @@ var paintInfo =
                         [
                             {
                                 "type": "rect",
-                                "width": 0.3,
+                                "width": (1/3),
                                 "height": 1,
-                                "x": 0.6,
+                                "x": (2/3),
                                 "y": 0
                             }
                         ],
                         [
                             {
                                 "type": "rect",
-                                "width": 0.3,
+                                "width": (1/3),
                                 "height": 1,
-                                "x": 0.3,
+                                "x": (1/3),
                                 "y": 0
                             }
                         ],
                         [
                             {
                                 "type": "rect",
-                                "width": 0.3,
+                                "width": (1/3),
                                 "height": 1,
                                 "x": 0,
                                 "y": 0
@@ -553,9 +553,262 @@ var paintInfo =
                 	]
                 }
             ]
+        },
+        {
+        	"name" : "overlay",
+        	"elements" : [
+        		{
+        			"name" : "empty",
+        			"displayName" : "none",
+        			"colors" : []
+        		},
+        		{
+        			"name" : "crossMiddle",
+        			"displayName" : "1/2 Split Cross",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"width" : 0.1,
+        						"height" : 1,
+        						"x" : 0.45,
+        						"y" : 0
+        					},
+        					{
+        						"type" : "rect",
+        						"width" : 1,
+        						"height" : 0.2,
+        						"x" : 0,
+        						"y" : 0.4
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "crossLeft",
+        			"displayName" : "1/3 Split Cross",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"width" : 0.1,
+        						"height" : 1,
+        						"x" : 0.3,
+        						"y" : 0
+        					},
+        					{
+        						"type" : "rect",
+        						"width" : 1,
+        						"height" : 0.2,
+        						"x" : 0,
+        						"y" : 0.4
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "stripeLTR",
+        			"displayName" : "Diagonal Stripe, Left To Right",
+        			"colors" : [
+    					[
+    						{
+    							"type" : "polygon",
+    							"points" : [
+    								[0,0],
+    								[(1/9),0],
+    								[1,(8/9)],
+    								[1,1],
+    								[(8/9),1],
+    								[0,(1/9)]
+    							]
+
+    						}
+    					]
+        			]
+        		},
+        		{
+        			"name" : "stripeRTL",
+        			"displayName" : "Diagonal Stripe, Right To Left",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "polygon",
+        						"points" : [
+        							[1,0],
+        							[(8/9),0],
+        							[0,(8/9)],
+        							[0,1],
+        							[(1/9),1],
+        							[1,(1/9)]
+        						]
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "crossStripe",
+        			"displayName" : "Double Diagional Stripe",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "polygon",
+        						"points" : [
+        							[1,0],
+        							[(8/9),0],
+        							[0,(8/9)],
+        							[0,1],
+        							[(1/9),1],
+        							[1,(1/9)]
+        						]
+        					},
+        					{
+    							"type" : "polygon",
+    							"points" : [
+    								[0,0],
+    								[(1/9),0],
+    								[1,(8/9)],
+    								[1,1],
+    								[(8/9),1],
+    								[0,(1/9)]
+    							]
+
+    						}	
+        				]
+        			]
+        		},
+        		{
+        			"name" : "halfTriangle",
+        			"displayName" : "Half Triangle",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "polygon",
+        						"points" : [
+        							[0,0],
+        							[0.5,0.5],
+        							[0,1]
+        						]
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "circle",
+        			"displayName" : "Circle",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "circle",
+        						"cx" : 0.5,
+        						"cy" : 0.5,
+        						"r" : (1/3)
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "diamond",
+        			"displayName" : "Diamond",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "polygon",
+        						"points" : [
+        							[0,0.5],
+        							[0.5,0],
+        							[1,0.5],
+        							[0.5,1]
+        						]
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "halfSquare",
+        			"displayName" : "Square, Half Length",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"squareSize" : 0.5,
+        						"x" : 0,
+        						"y" : 0
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "halfWidthSquare",
+        			"displayName" : "Square, Half Length, Half Width",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"height" : 0.5,
+        						"width" : 0.5,
+        						"x" : 0,
+        						"y" : 0
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "halfSquare",
+        			"displayName" : "Square, 2/3 Height",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"squareSize" : (2/3),
+        						"x" : 0,
+        						"y" : 0
+        					}
+        				]
+        			]
+        		},
+        		{
+        			"name" : "halfWidthSquare",
+        			"displayName" : "Square, 2/3 height 1/2 width",
+        			"colors" : [
+        				[
+        					{
+        						"type" : "rect",
+        						"height" : (2/3),
+        						"width" : 0.5,
+        						"x" : 0,
+        						"y" : 0
+        					}
+        				]
+        			]
+        		},
+        	]
         }
     ]
 }
+
+var icons = 
+{
+    "categories": [
+        {
+            "name": "Shapes",
+            "subCategories": [
+                {
+                    "name": "stars",
+                    "elements": [
+                        {
+                        	"name": "star-1",
+                            "type": "path",
+                            "ID": "star-2-outline",
+                            "d": "M256,128.858l42.98,88.667l97.61,13.478l-71.047,68.278l17.346,96.996L256,349.809l-86.89,46.469l17.346-96.996l-71.047-68.278l97.61-13.478L256,128.858z M256,60.082l-62.978,129.92L49.999,209.75L154.1,309.793l-25.415,142.125L256,383.828l127.315,68.09L357.9,309.793L462.001,209.75l-143.023-19.748L256,60.082z"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+var testElement = '<path id="star-2-outline-icon" d="M256,128.858l42.98,88.667l97.61,13.478l-71.047,68.278l17.346,96.996L256,349.809l-86.89,46.469l17.346-96.996l-71.047-68.278l97.61-13.478L256,128.858z M256,60.082l-62.978,129.92L49.999,209.75L154.1,309.793l-25.415,142.125L256,383.828l127.315,68.09L357.9,309.793L462.001,209.75l-143.023-19.748L256,60.082z"/>'
 
 var previewWindowInfo = 
 {
@@ -591,16 +844,40 @@ function initUI(){
 		
 		html.push("</div>");
 	}
+	//create the icon UI
+	html.push("<h3>Flag Icon</h3>");
+	html.push("<div class='btn-group'>");
+	for(c=0;c<icons.categories.length;c++){
+		html.push("<button type='button' class='btn btn btn-default dropdown-toggle' data-toggle='dropdown'>" + icons.categories[c].name + " <span class='caret'></span></button>");
+		html.push("<ul class='dropdown-menu' role='menu'>");
+		for(v=0;v<icons.categories[c].subCategories.length;v++){
+			html.push("<li role='presentation' class='dropdown-header'>" + icons.categories[c].subCategories[v].name + "</li>");
+			for(b=0;b<icons.categories[c].subCategories[v].elements.length;b++){
+				html.push("<li class='iconChoice' data-icon-Category='"+c+"' data-icon-Subcategory='"+v+"' data-icon-Element='"+b+"'>"+icons.categories[c].subCategories[v].elements[b].name + "<li>");
+			}
+		}
+		html.push("</ul>")
+	}
+	html.push("</div>");
 	console.log(html);
 	var content;
 	for(i=0;i<html.length;i++){
 		content = content + html[i];
 	}
 	$("#controlPane").append(content);
-
+	bindIconChoice();
 	//create the preview pane
 	$("#previewPane").append("<div id='previewPicture'></div><div id='previewControls'></div>");
 	updateImage();
+}
+ var selectedIcon = {};
+function bindIconChoice(){
+	$(".iconChoice").click(function(){
+		console.log($(this).data());
+		data = $(this).data();
+		selectedIcon = data;
+		updateImage();
+	});
 }
 
 function elementButtonClick(layerNum,elementNum){
@@ -679,7 +956,7 @@ function updateImage(){
 
 function generateSVG(){
 	svg = new Array();
-	svg.push("<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.0' x='0' y='0' width='"+previewWindowInfo.width+"' height='"+previewWindowInfo.height + "'>");
+	svg.push("<svg viewBox='0 0 350 200' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.0' x='0' y='0' width='"+previewWindowInfo.width+"' height='"+previewWindowInfo.height + "'>");
 	//cycle through each layer
 	for(x=0;x<dataArray.length;x++){
 		//cycle through the array
@@ -697,13 +974,43 @@ function generateSVG(){
 	}
 
 	//svg.push("<rect width='"+previewWindowInfo.width+"' height='"+previewWindowInfo.height + "' x='0' y='0' fill='#333333' />");
-
+	//add the icon svg
+	iconSvg = generateIconSVG();
+	svg.push(iconSvg);
 	svg.push("</svg>");
 	var content = "";
 	for(x=0;x<svg.length;x++){
 		content = content + svg[x];
 	}
 	return content;
+}
+
+function generateIconSVG(){
+	iconObject = icons.categories[selectedIcon.iconCategory].subCategories[selectedIcon.iconSubcategory].elements[selectedIcon.iconElement];
+	element = new Array();
+	element.push("<svg width = '" + previewWindowInfo.width + "' height = '" + previewWindowInfo.height + "' x='0' y='0' viewBox = '15 0 500 500'>") //opening group
+	element.push("<");
+	element.push(iconObject.type + " ")
+	for(var key in iconObject){
+		if(iconObject.hasOwnProperty(key)){
+			var pushString = "";
+			switch(key){
+				case "name":
+				//do nothing
+					break;
+				case "ID":
+					pushString = "ID = '" + iconObject[key] + "' ";
+					break;
+				case "d":
+					pushString = "d = '" + iconObject[key] + "' ";
+			}
+			element.push(pushString);
+		}
+	}
+	element.push("class='flag-icon' />");
+	element.push("</g>") //closing the icon/group
+	sendString = compileArray(element);
+	return sendString;
 }
 
 function generateSVGLayer(colorObject,colorHex){
@@ -734,6 +1041,18 @@ function generateSVGLayer(colorObject,colorHex){
 				case "points":
 					pointString = getPointsStringFromArray(colorObject[key]);
 					pushString = "points= '" + pointString + "' ";
+					break;
+				case "cx":
+					pushString = "cx = '" + (previewWindowInfo.width * colorObject[key]) + "' ";
+					break;
+				case "cy":
+					pushString = "cy = '" + (previewWindowInfo.height * colorObject[key]) + "' ";
+					break;
+				case "r":
+					pushString = "r = '" + (previewWindowInfo.height * colorObject[key]) + "' ";
+					break;
+				case "squareSize":
+					pushString = "width = '" + (previewWindowInfo.height * colorObject[key]) + "' height = '" + (previewWindowInfo.height * colorObject[key]) + "' ";
 					break;
 		    }
 		    element.push(pushString);
